7c353527bb8a010a24ab13e72993c7e76999ffca
VISHAL-1602 <vishalmahalakshmi4@gmail.com>
2024-08-05 23:26:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Element } from 'react-scroll';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Testimonials from './components/Testimonials';
+import ProjectDetail from './components/ProjectDetail';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <div className="container mt-5 pt-5">
+          <Routes>
+            <Route path="/" element={<>
+              <Element name="home"><Home /></Element>
+              <Element name="about"><About /></Element>
+              <Element name="projects"><Projects /></Element>
+              <Element name="skills"><Skills /></Element>
+              <Element name="contact"><Contact /></Element>
+              <Element name="testimonials"><Testimonials /></Element>
+            </>} />
+            <Route path="/project/:projectId" element={<ProjectDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
